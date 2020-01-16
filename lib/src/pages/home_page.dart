907dc701a3c941a70c26c0ff5b2a2cbc757ba358
lodi <lodi.dev@outlook.com>
2020-01-15 22:43:15 -0600

    icnos and other  things

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:beats/src/widgets/fondo_circular.dart';
+import 'package:beats/src/widgets/CustomAppBar.dart';
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola  mundo'),
-      ),
-    );
+        body: Stack(
+      children: <Widget>[
+        FondoCicular(),
+        SafeArea(
+          child: SingleChildScrollView(child: Column(children: <Widget>[CustomAppBar()],),),
+        )
+      ],
+    ));
   }
 }
+
